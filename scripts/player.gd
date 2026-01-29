f73c327cf148a31a6cb927219be9630f15247df4
djohoe28 <djohoe28@gmail.com>
2026-01-30 01:35:55 +0200

    Refactored Player movement; added Pushable group.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,38 @@
 extends StaticBody2D
 
+signal moved
+signal pushed
+signal blocked
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const ActionToVector = {
+	"ui_left": Vector2.LEFT,
+	"ui_right": Vector2.RIGHT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN
+}
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if not Input.is_action_just_pressed("ui_left") and not Input.is_action_just_pressed("ui_right") and not Input.is_action_just_pressed("ui_up") and not Input.is_action_just_pressed("ui_down"):
-		$RayCast2D.target_position = Vector2.ZERO
-		return
-	var xinput := Input.get_axis("ui_left", "ui_right")
-	var yinput := Input.get_axis("ui_up", "ui_down")
-	# Prevent diagonal movement (prioritize horizontal movement)
-	if xinput != 0:
-		yinput = 0
-	$RayCast2D.target_position = Vector2(xinput, yinput) * Globals.TILE_SIZE
+func try_move(direction: Vector2) -> void:
+	# Check for collision
+	$RayCast2D.target_position = direction * Globals.TILE_SIZE
 	$RayCast2D.force_raycast_update()
+	# Move if no collision
 	if not $RayCast2D.is_colliding():
-		position += Vector2(xinput, yinput) * Globals.TILE_SIZE
+		position += direction * Globals.TILE_SIZE
+		emit_signal("moved")
+		print("Player moved to ", position)
 	else:
-		print("Collision detected!")
+		if $RayCast2D.get_collider().is_in_group("Pushable"):
+			# TODO: Push the object first!
+			emit_signal("pushed", $RayCast2D.get_collider(), direction)
+			# TODO: Move player if push successful(?)
+			# position += direction * Globals.TILE_SIZE
+			print("Player pushed an object at ", $RayCast2D.get_collider().position)
+		else:
+			emit_signal("blocked")
+			print("Player movement blocked at ", position + direction * Globals.TILE_SIZE)
+
+func _unhandled_input(_event: InputEvent) -> void:
+	# Handle movement input; Order is determined by `ActionToVector.keys()`.
+	for action in ActionToVector.keys():
+		if Input.is_action_just_pressed(action):
+			try_move(ActionToVector[action])
