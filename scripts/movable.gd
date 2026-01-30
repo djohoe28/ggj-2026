3b5c24c9b9f8b11a85817cb34e98a56be5e8a8f0
djohoe28 <djohoe28@gmail.com>
2026-01-30 05:48:05 +0200

    movable.gd linting.

--- scripts/movable.gd
+++ scripts/movable.gd
@@ -1,13 +1,24 @@
 extends StaticBody2D
 class_name Movable
 
+#region Signals
+
+## The Movable has successfully moved.
 signal moved(movable: Movable, direction: Vector2)
+
+## The Movable's movement was blocked by a collider.
 signal blocked(movable: Movable, collider: Node2D, direction: Vector2)
 
+#endregion
+
+#region Methods
+
+## Move the Movable in the specified direction.
 func move(direction: Vector2) -> void:
 	position += direction * Globals.TILE_SIZE
 	moved.emit(self, direction)
 
+## Attempt to move the Movable in the specified direction.
 func try_move(direction: Vector2) -> bool:
 	# Check for collision
 	$RayCast2D.target_position = direction * Globals.TILE_SIZE
@@ -19,3 +30,5 @@ func try_move(direction: Vector2) -> bool:
 	
 	blocked.emit(self, $RayCast2D.get_collider(), direction)
 	return false
+
+#endregion
