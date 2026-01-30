eaff73e9a67cdf5ef28ffd2d5a4fc1d4d3989194
djohoe28 <djohoe28@gmail.com>
2026-01-30 03:20:12 +0200

    Updated Player logic with pushing signals.

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ signal moved
 signal pushed
 signal blocked
 
+@export var moves_after_push: bool = true
+
 const ActionToVector = {
 	"ui_left": Vector2.LEFT,
 	"ui_right": Vector2.RIGHT,
@@ -11,25 +13,30 @@ const ActionToVector = {
 	"ui_down": Vector2.DOWN
 }
 
+func move(direction: Vector2) -> void:
+	position += direction * Globals.TILE_SIZE
+	emit_signal("moved")
+
 func try_move(direction: Vector2) -> void:
 	# Check for collision
 	$RayCast2D.target_position = direction * Globals.TILE_SIZE
 	$RayCast2D.force_raycast_update()
 	# Move if no collision
 	if not $RayCast2D.is_colliding():
-		position += direction * Globals.TILE_SIZE
-		emit_signal("moved")
-		print("Player moved to ", position)
+		move(direction)
 	else:
-		if $RayCast2D.get_collider().is_in_group("Pushable"):
-			# TODO: Push the object first!
-			emit_signal("pushed", $RayCast2D.get_collider(), direction)
-			# TODO: Move player if push successful(?)
-			# position += direction * Globals.TILE_SIZE
-			print("Player pushed an object at ", $RayCast2D.get_collider().position)
+		var collider = $RayCast2D.get_collider() as Node2D
+		# If colliding with a pushable object, try to push it
+		if collider.is_in_group("Pushable"):
+			collider.call("try_push", direction)
+			emit_signal("pushed", collider, direction)
+			# Move after pushing if toggled
+			if moves_after_push:
+				move(direction)
+			print(name, " pushed ", collider.name)
 		else:
 			emit_signal("blocked")
-			print("Player movement blocked at ", position + direction * Globals.TILE_SIZE)
+			print(name, " blocked by ", collider.name)
 
 func _unhandled_input(_event: InputEvent) -> void:
 	# Handle movement input; Order is determined by `ActionToVector.keys()`.
